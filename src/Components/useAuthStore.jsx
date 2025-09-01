6d74e5b03f665a122989efbc7b6341499cddf010
Serob <sayadyanserob04@gmail.com>
2025-09-01 10:56:39 +0400

    firebase

--- src/Components/useAuthStore.jsx
+++ src/Components/useAuthStore.jsx
@@ -1,4 +1,3 @@
-
 import { create } from "zustand";
 
 const useAuthStore = create((set) => ({
@@ -20,14 +19,19 @@ const useAuthStore = create((set) => ({
     localStorage.removeItem("user");
     set({ user: null });
   },
-   loginFromAPI: async (userId) => {
+  loginFromAPI: async (userId) => {
     try {
-      const res = await fetch(`https://68af739ab91dfcdd62bc5866.mockapi.io/api/v1/users/${userId}`);
+      const res = await fetch(
+        `https://68af739ab91dfcdd62bc5866.mockapi.io/api/v1/users/${userId}`
+      );
       if (!res.ok) throw new Error("Failed to fetch user");
       const data = await res.json();
-     
+
       set({ user: { id: data.id, name: data.name } });
-      localStorage.setItem("user", JSON.stringify({ id: data.id, name: data.name }));
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ id: data.id, name: data.name })
+      );
     } catch (err) {
       console.error(err);
     }
